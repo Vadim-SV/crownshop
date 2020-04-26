4553461a2e21ef909ffe781d22cec7c0206605e5
Vadim-SV <syubaew@gmail.com>
2020-04-26 19:30:42 +0100

    Initial commit / backup

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss';
+import {Switch , Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+       <Switch>
+            <Route exact path = '/' component = {HomePage}/>
+            <Route path='/shop' component={ShopPage}/>
+            <Route path='/signin' component={SignInAndSignUpPage}/>
+
+       </Switch>
+    
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss'
+
+import SignIn from '../../components/signin/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+  <div>
+  
+    <SignIn/>
+
+  </div>
+
+
+
+);
+
+export default SignInAndSignUpPage;
